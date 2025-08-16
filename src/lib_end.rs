1a8c2836ad790f193f7581a214d35f09f86122c8
TheCukitoDev <145783815+TheCukitoDev@users.noreply.github.com>
2025-08-16 20:39:15 +0200

    Clean unupdated files

--- src/lib_end.rs
+++
@@ -1,7 +0,0 @@
-    #[test]
-    fn test_type_info_basic() {
-        let u64_info = extract_type_info::<u64>();
-        assert_eq!(u64_info.rust_type, "u64");
-        assert_eq!(u64_info.json_format, "integer");
-    }
-}
